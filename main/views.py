6493c996f99b807eabf1332130cb9be272383ffa
Egor14 <egorgrekhnev@mail.ru>
2019-05-25 21:49:10 +0300

    send opt route to Stas

--- main/views.py
+++ main/views.py
@@ -15,22 +15,29 @@ def hello(request):
 
 
 def route(request):
+    # print(request.GET.get('places'))
+    places = json.loads(request.GET.get('places'))
+
     API_KEY = 'f829f73b-e5a2-4c8f-acd2-ed6e84a04a73'
     API_ROOT_ENDPOINT = 'https://courier.common.yandex.ru/vrs/api/v1'
 
     # Создайте тело запроса, включая склад, точки доставки, транспортное средство и настройки.
     payload = {
         'depot': {'id': 0, 'time_window': '07:00-23:59', 'point': {'lat': 59.55971, 'lon': 30.102793}},
-        'locations': [
-            {'id': 1, 'time_window': '09:00-20:00', 'point': {'lat': 59.567352, 'lon': 30.100999}},
-            {'id': 2, 'time_window': '15:00-20:00', 'point': {'lat': 59.56954, 'lon': 30.11393}},
-            # {'id': 3, 'time_window': '12:00-15:00', 'point': {'lat': 55.809657, 'lon': 37.520314}},
-            # {'id': 4, 'time_window': '09:00-15:00', 'point': {'lat': 55.744764, 'lon': 37.558224}},
-            # {'id': 5, 'time_window': '15:00-20:00', 'point': {'lat': 55.788563, 'lon': 37.670101}},
-        ],
+        'locations': [],
         'vehicle': {'id': 0},
         'options': {'time_zone': 3}
     }
+
+    names = []
+
+    for i, place in enumerate(places):
+        payload['locations'].append(
+            {'id': i + 1, 'time_window': '09:00-20:00', 'point': {'lat': place['lat'], 'lon': place['lon']}})
+        names.append(place['name'])
+
+    print(payload)
+    print(names)
 
     # Отправьте запрос и получите ID поставленной задачи.
     response = requests.post(
@@ -87,9 +94,9 @@ def route(request):
                 print(yamaps_url)
 
                 print('Порядок ', ids)
-                return HttpResponse('кажется что то работает')
+                return JsonResponse({})
 
-    return HttpResponse('вообще хз что происходит')
+    return JsonResponse({})
 
 
 def build(request):
@@ -183,6 +190,5 @@ def food(request):
         else:
 
             print('мда')
-
 
     return HttpResponse('lol')
