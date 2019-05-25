ec019f1c4eefc8adcd6470bb8a1d6cc483129a19
MenacingDwarf <MenacingDwarf@yandex.ru>
2019-05-25 14:59:55 +0300

    Front Pages

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,32 +1,48 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 
 class Home extends Component {
-    state = {
-        text: 'Loading...'
-    };
-
-    componentDidMount() {
-        let getText = (comp) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open("GET", "http://127.0.0.1:8000/", true);
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            xhr.onreadystatechange = function () {
-                if (this.readyState === 4) {
-                    comp.setState({
-                        text: JSON.parse(this.responseText).text
-                    })
-                }
-            };
-
-            xhr.send();
-        };
-
-        getText(this);
-    }
 
     render() {
         return (
-            <h1>{this.state.text}</h1>
+            <div>
+                <div className="row">
+                    <div id="carouselExampleControls" className="carousel slide w-75 mx-auto" data-ride="carousel">
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <img src="http://1945.sputnik.ru/img/city/gatchina.jpg" className="d-block w-100"
+                                     alt="..."/>
+                            </div>
+                            <div className="carousel-item">
+                                <img
+                                    src="http://www.museums.severstal.com/wp-content/uploads/2016/01/GMZ-Gatchina-720x445.jpg"
+                                    className="d-block w-100" alt="..."/>
+                            </div>
+                        </div>
+                        <a className="carousel-control-prev" href="#carouselExampleControls" role="button"
+                           data-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="sr-only">Previous</span>
+                        </a>
+                        <a className="carousel-control-next" href="#carouselExampleControls" role="button"
+                           data-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+                <div className="row">
+                    <center>
+                        <div className="h3">Добро пожаловать в Гатчину!</div>
+                        <p>Мы привествуем всех гостей Гатчины и хотим предложить вам сконструировать свою
+                            персонализированную прогулку по прекрасному городу Гатчина!</p>
+                        <Link to="/route">
+                            <button className="btn btn-primary">Подготовить маршрут</button>
+                        </Link>
+                    </center>
+                </div>
+
+            </div>
         )
     }
 }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom'
 class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light mb-1">
+                <div className="collapse navbar-collapse container" id="navbarSupportedContent">
+                    <ul className="nav ml-auto">
                         <li className="nav-item">
-                            <Link className="nav-link" to="/">Home <span className="sr-only">Home</span></Link>
+                            <Link className="nav-link" to="/">Главная<span className="sr-only">Home</span></Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/contacts">Contacts<span className="sr-only">Home</span></Link>
+                            <Link className="nav-link" to="/contacts">Контакты<span className="sr-only">Home</span></Link>
                         </li>
                     </ul>
                 </div>
