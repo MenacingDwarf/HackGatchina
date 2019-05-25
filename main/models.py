f443e137264d8b0fe3a0767172d2abe658908292
Egor14 <egorgrekhnev@mail.ru>
2019-05-25 12:22:48 +0300

    table register and route request

--- main/models.py
+++ main/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Sight(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
-    price = models.IntegerField()
-    work_start = models.TimeField()
-    work_end = models.TimeField()
+    price = models.IntegerField(default=0)
+    work_start = models.TimeField(default='08:00:00')
+    work_end = models.TimeField(default='17:00:00')
     lat = models.FloatField()
     lon = models.FloatField()
     photo = models.CharField(max_length=150)
--- main/views.py
+++ main/views.py
@@ -22,7 +22,7 @@ def route(request):
             {'id': 2, 'time_window': '15:00-20:00', 'point': {'lat': 55.739796, 'lon': 37.689102}},
             {'id': 3, 'time_window': '12:00-15:00', 'point': {'lat': 55.809657, 'lon': 37.520314}},
             {'id': 4, 'time_window': '09:00-15:00', 'point': {'lat': 55.744764, 'lon': 37.558224}},
-            {'id': 5, 'time_window': '15:00-20:00', 'point': {'lat': 55.788563, 'lon': 37.670101}},
+            #{'id': 5, 'time_window': '15:00-20:00', 'point': {'lat': 55.788563, 'lon': 37.670101}},
         ],
         'vehicle': {'id': 0},
         'options': {'time_zone': 3}
@@ -33,6 +33,61 @@ def route(request):
         API_ROOT_ENDPOINT + '/add/svrp',
         params={'apikey': API_KEY}, json=payload)
 
-    print(response)
-    return HttpResponse(response)
 
+
+
+    # Дождитесь ответа.
+    poll_stop_codes = {
+        requests.codes.ok,
+        requests.codes.gone,
+        requests.codes.internal_server_error
+    }
+
+    # Опрос сервера о готовности результата оптимизации маршрута с использованием полученного ранее ID.
+    if response.status_code == requests.codes.accepted:
+        request_id = response.json()['id']
+        poll_url = '{}/result/svrp/{}'.format(API_ROOT_ENDPOINT, request_id)
+
+        response = requests.get(poll_url)
+        while response.status_code not in poll_stop_codes:
+            time.sleep(1)
+            response = requests.get(poll_url)
+
+
+
+
+        # Вывод информации в пользовательском формате.
+        if response.status_code != 200:
+            print('Error {}: {}'.format(response.text, response.status_code))
+            return HttpResponse('что то сломалось')
+        else:
+            print('Route optimization completed')
+            print('')
+
+            for route in response.json()['result']['routes']:
+                print('Vehicle {} route: {:.2f}km'.format(
+                    route['vehicle_id'], route['metrics']['total_transit_distance_m'] / 1000))
+
+
+                # Вывод маршрута в текстовом формате.
+                for waypoint in route['route']:
+                    print('  {type} {id} at {eta}, {distance:.2f}km driving '.format(
+                        type=waypoint['node']['type'],
+                        id=waypoint['node']['value']['id'],
+                        eta=str(datetime.timedelta(seconds=waypoint['arrival_time_s'])),
+                        distance=waypoint['transit_distance_m'] / 1000))
+
+                # Вывод маршрута в формате ссылки на Яндекс.Карты.
+                yamaps_url = 'https://yandex.ru/maps/?mode=routes&rtext='
+                for waypoint in route['route']:
+                    point = waypoint['node']['value']['point']
+                    yamaps_url += '{}%2c{}~'.format(point['lat'], point['lon'])
+
+                print('')
+                print('See route on Yandex.Maps:')
+                print(yamaps_url)
+
+                return HttpResponse('кажется что то работает')
+
+
+    return HttpResponse('вообще хз что происходит')
